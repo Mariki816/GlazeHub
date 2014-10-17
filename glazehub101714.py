02864b5c1d293a8924923ae67603cdb697dfb142
Mariki816 <marlenehirose@gmail.com>
2014-10-17 15:30:10 -0700

    Added function to ask for number of ingredients. Functionalized getting recipe. Return dictionary

---
+++ glazehub101714.py
@@ -0,0 +1,59 @@
+def kilocalc(vol):
+    percentvol = vol/100.0
+    kilo = (percentvol * 1000.0)
+
+    return kilo
+
+def purpleGlaze():
+    print "Purple Glaze Recipe"
+    recipe = {  
+        "pf" : 29.3,
+        "sil" : 24.1,
+        "wh" : 9.0,
+        "kao" : 6.8,
+        "dol" : 6.8,
+        "talc" : 13.5,
+        "gbor" : 10.5,
+        "zirc" : 8.0,
+        "color" : 1.0,
+        }
+
+    for i in recipe:
+        result = kilocalc(recipe.get(i))
+        print i, "=", result, "grams"
+
+    return purpleGlaze
+
+
+def howManyforBase():
+    print "How many ingredients for base? "
+    num = raw_input()
+
+    return num
+
+def GetIngredients(num):
+    d = {}
+    for i in range(num):
+        print "Chem name: "
+        chem = raw_input()
+        print "Volume(percentage)"
+        weight = raw_input()
+        d[chem] = float(weight)
+    return d
+
+
+def main():
+    #purpleGlaze()
+
+    x = howManyforBase()
+    print "%s" % x
+    xint = int (x)
+    print GetIngredients(xint)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
+ 
